8f4bea88fcdb438e05d48826a2c7cc852f10229a
samarkun <samarkun4@gmail.com>
2025-04-11 21:03:44 -0700

    adding a method of slice method

--- frontend/src/components/LatestBlogs.jsx
+++ frontend/src/components/LatestBlogs.jsx
@@ -6,7 +6,7 @@ const LatestBlogs = () => {
         <div>
             <h1 className="text-3xl my-3 font-bold text-center sm:text-start">Latest Blogs</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-3 sm:px-4">
-                {blogData.map((blog, index) => (
+                {blogData.slice(-4).reverse().map((blog, index) => (  //its show only 4 blog like -4 .reverse
                     <BlogCard
                         key={index}
                         id={blog.id}
