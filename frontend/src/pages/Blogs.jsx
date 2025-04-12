21faf52cb9fabfda983385cd357f94f3d18ca39b
samarkun <samarkun4@gmail.com>
2025-04-11 21:37:13 -0700

    responsivness improve

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -8,7 +8,7 @@ const Blogs = () => {
         <div>
             <Hero />
             <h1 className='text-3xl text-center font-bold my-6'>All Blogs</h1>
-            <p className='text-lg leading-6 max-w-2xl mx-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos ratione doloribus, enim animi commodi illum ipsam impedit quas blanditiis laborum, vitae perferendis ipsum? Ratione reprehenderit magnam officiis! Magni, dicta saepe.
+            <p className='text-base px-3 sm:text-lg leading-6 max-w-2xl mx-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos ratione doloribus, enim animi commodi illum ipsam impedit quas blanditiis laborum, vitae perferendis ipsum? Ratione reprehenderit magnam officiis! Magni, dicta saepe.
             Accusantium rem omnis sunt? Voluptas, quidem voluptate voluptatum tenetur illo eaque fugit natus, corporis non quia deleniti praesentium facilis odit minus totam mollitia itaque reiciendis eum recusandae similique accusamus sit!</p>
             <div className="grid my-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-3 sm:px-4">
                 {blogData.map((blog, index) => (
